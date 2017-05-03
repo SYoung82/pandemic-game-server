bcacf51ca38dcb0ceb4bb3f0f421d0ba8930e38f
Scott Young <young.scottw@gmail.com>
2017-05-03 10:40:13 -0500

    Fixed UsersController to return json on show request

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,13 +2,19 @@ class Api::V1::UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        
+
+        binding.pry
         if @user
-            render @user
+            render json: @user
         else
             render json: {
                 errors: @user.errors
             }, status: 500
         end
     end
+
+    def create
+
+    end
+    
 end
