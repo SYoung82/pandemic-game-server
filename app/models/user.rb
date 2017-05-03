f899fbe171e7f2d8dfbfa75e77223f12d9f34438
Scott Young <young.scottw@gmail.com>
2017-05-03 12:12:11 -0500

    Migrate db

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
     validates :email, presence: true
     validates :email, uniqueness: true
     validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-
 end
