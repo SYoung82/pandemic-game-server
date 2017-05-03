06eb7213024fb336f6160a06aab4d4936221e9a7
Scott Young <young.scottw@gmail.com>
2017-05-03 12:30:37 -0500

    Implement UsersController#Create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,7 +13,21 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
 
+        if @user.save
+            render 'users/user.json.jbuilder', user: @user
+        else
+            render json: {
+                @user.errors
+            }, status: 500
+        end
+    end
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
     end
     
 end
