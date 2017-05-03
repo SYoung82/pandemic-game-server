8ad6980c18d91069d2ee7c2c855a40d0c0ea2856
Scott Young <young.scottw@gmail.com>
2017-05-03 15:44:43 -0500

    Make games routes nested under users routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-      resources :users, only: [:create, :show]
-
-      resources :games, only: [:show]
+      resources :users, only: [:create, :show] do
+        resources :games, only: [:show, :index]
+      end
       
       post '/auth', to: "auth#login"
       post '/auth/refresh', to: "auth#refresh"
