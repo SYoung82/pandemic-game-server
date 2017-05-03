ca5bfa532228e7bbc6981b93b70613d20867c622
Scott Young <young.scottw@gmail.com>
2017-05-03 17:03:43 -0500

    GamesController#index only renders games if owned by authenticated user

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,13 +2,15 @@ class Api::V1::GamesController < ApplicationController
     before_action :authenticate_token!, only: [:show, :index]
 
     def index
-        @games = Game.find_by(user_id: params[:user_id])
-
+        if current_user.id.to_s == params[:user_id]
+            @games = Game.where(user_id: params[:user_id])
+        end
+        
         if @games
             render json: @games
         else
             render json: {
-                errors: @games.errors
+                errors: 'Invalid credentials to view these games'
             }, status: 500
         end
     end
