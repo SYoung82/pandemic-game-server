f67150113f7e00d8ec55ea1e0945350c74092402
Scott Young <young.scottw@gmail.com>
2017-05-03 12:09:50 -0500

    Built user model out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_many :games, dependent: :destroy
+
+    has_secure_password
+
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
 end
